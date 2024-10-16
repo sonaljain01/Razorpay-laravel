6133d3e3c828486615529591e03605cdde0b6f7c
sonaljain01 <jainsonal8630407466@gmail.com>
2024-10-16 12:53:08 +0530

    fix: update the routes

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,16 @@ Route::get('/user', function (Request $request) {
 
 
 
-Route::group(['prefix' => 'user'], function () {
-    // Route::group(['middleware' => 'auth:api'], function () {
-        Route::post('payment/create-order', [PaymentgatewayController::class, 'createOrder']);
-        Route::post('payment/cancel', [PaymentgatewayController::class, 'cancelOrder']);
-        Route::post('webhook/razorpay', [PaymentgatewayController::class, 'handleWebhook']);
-        Route::post('payment/verify', [PaymentgatewayController::class, 'verify']);
-    });
+// Route::group(['prefix' => 'user'], function () {
+// Route::group(['middleware' => 'auth:api'], function () {
+Route::post('payment/create-order', [PaymentgatewayController::class, 'createOrder']);
+Route::post('payment/cancel', [PaymentgatewayController::class, 'cancelOrder']);
+Route::post('webhook/razorpay', [PaymentgatewayController::class, 'handleWebhook']);
+Route::post('payment/verify', [PaymentgatewayController::class, 'verify']);
 // });
+// });
+Route::get('webhook/razorpay/', function () {
+    return response()->json([
+        'message' => 'No Reaponse',
+    ]);
+});
